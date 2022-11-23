420994ac9e8e13d8419296ac24daf16ab7c9dae9
Vicent Gisbert Soto <vicgisso@gmail.com>
2022-11-23 07:25:34 +0100

    example test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,12 +2,17 @@ import { shallowMount } from '@vue/test-utils'
 import BaseButton from '@/components/BaseButton.vue'
 
 describe('BaseButton.vue', () => {
-  it('renders a button and click it', () => {
-    // const onClick = jest.fn();
-    // const wrapper = shallowMount(BaseButton, {
-    //   props: { onClick },
-    // })
-    // expect(wrapper.text()).toMatch(msg)
-    expect(true).toBeTruthy();
+  it('renders a button with inner text and click it', () => {
+    const onClick = jest.fn();
+    const buttonText = 'button';
+    const wrapper = shallowMount(BaseButton, {
+      props: { onClick },
+      slots: { default: buttonText },
+    });
+
+    wrapper.trigger('click');
+
+    expect(wrapper.text()).toBe(buttonText);
+    expect(onClick).toHaveBeenCalledTimes(1);
   })
 })
