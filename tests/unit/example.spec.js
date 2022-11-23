3d06b5c64c4dae4825044c3af99bb67750388c39
Vicent Gisbert Soto <vicgisso@gmail.com>
2022-11-23 07:14:50 +0100

    jest + vue test utils Setup

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import BaseButton from '@/components/BaseButton.vue'
+
+describe('BaseButton.vue', () => {
+  it('renders a button and click it', () => {
+    // const onClick = jest.fn();
+    // const wrapper = shallowMount(BaseButton, {
+    //   props: { onClick },
+    // })
+    // expect(wrapper.text()).toMatch(msg)
+    expect(true).toBeTruthy();
+  })
+})
