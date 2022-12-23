e670772e57c6c5a1e26b3f04347ad453c53db24c
Borja Ferrer <borjetaa27@gmail.com>
2022-12-23 13:14:01 +0100

    Added BaseTextArea component to the form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="form">
     <header>
-        <Explanation :title="title" :subtitle="subtitle" />
+      <Explanation :title="title" :subtitle="subtitle" />
     </header>
-  
+
     <main>
       <div class='row'>
         <div class='column'>
@@ -13,17 +13,23 @@
           <BaseInput label="Surnames" />
         </div>
       </div>
-    
+
       <div class='row'>
         <div class='column'>
           <BaseInput label="Address" />
         </div>
         <div class='column'>
+          <BaseTextArea label="Another thing" id="1" cols="1" rows="3" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
           <BaseInput label="Accept conditions" type="checkbox" />
         </div>
       </div>
     </main>
-  
+
     <footer>
       <BaseButton :onClick="click">Submit</BaseButton>
     </footer>
@@ -34,6 +40,7 @@
 import Explanation from './Explanation.vue';
 import BaseInput from './BaseInput.vue';
 import BaseButton from './BaseButton.vue';
+import BaseTextArea from './BaseTextArea.vue';
 
 const title = 'Testing Form';
 const subtitle = 'This form is created with testable components.';
@@ -44,21 +51,21 @@ const click = () => {
 </script>
 
 <style scoped>
-  .form {
-    width: 700px;
-  }
+.form {
+  width: 700px;
+}
 
-  header {
-    line-height: 1.5;
-  }
+header {
+  line-height: 1.5;
+}
 
-  main {
-    margin: 20px 0 20px 0;
-  }
-  
-  footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+main {
+  margin: 20px 0 20px 0;
+}
+
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
