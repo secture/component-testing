5b4bab7b49e50aed6c9308632721214b8fa78df6
Borja Ferrer <borjetaa27@gmail.com>
2022-12-23 12:34:55 +0100

    Added BaseTextArea component

---
+++ src/components/BaseTextArea.vue
@@ -0,0 +1,20 @@
+<template>
+    <label :id="id">{{ label }}</label>
+    <textarea :id="id" :cols="cols" :rows="rows" :maxlength="maxLength" :minlength="minLength"
+        :placeholder="content"></textarea>
+</template>
+
+<script>
+
+export default {
+    props: {
+        cols: Number,
+        rows: Number,
+        id: String,
+        minLength: Number,
+        maxLength: Number,
+        content: String,
+        label: String
+    }
+}
+</script>
