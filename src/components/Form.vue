dafd33486521f3e64e54f3137599bee5118ce8d8
Borja Ferrer <borjetaa27@gmail.com>
2023-01-02 14:01:07 +0100

    Added progress bar component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,9 +1,10 @@
 <template>
+  <progress-bar maxSteps="3" :step="stepNumber" />
   <div class="form">
     <header>
-        <Explanation :title="title" :subtitle="subtitle" />
+      <Explanation :title="title" :subtitle="subtitle" />
     </header>
-  
+
     <main>
       <div class='row'>
         <div class='column'>
@@ -13,7 +14,7 @@
           <BaseInput label="Surnames" />
         </div>
       </div>
-    
+
       <div class='row'>
         <div class='column'>
           <BaseInput label="Address" />
@@ -23,9 +24,9 @@
         </div>
       </div>
     </main>
-  
+
     <footer>
-      <BaseButton :onClick="click">Submit</BaseButton>
+      <BaseButton :onClick="click">Next</BaseButton>
     </footer>
   </div>
 </template>
@@ -34,31 +35,35 @@
 import Explanation from './Explanation.vue';
 import BaseInput from './BaseInput.vue';
 import BaseButton from './BaseButton.vue';
+import ProgressBar from './ProgressBar.vue';
+import { ref } from 'vue';
 
 const title = 'Testing Form';
 const subtitle = 'This form is created with testable components.';
 
+const stepNumber = ref(1)
 const click = () => {
+  stepNumber.value++
   return console.log('button clicked!');
 }
 </script>
 
 <style scoped>
-  .form {
-    width: 700px;
-  }
+.form {
+  width: 700px;
+}
 
-  header {
-    line-height: 1.5;
-  }
+header {
+  line-height: 1.5;
+}
 
-  main {
-    margin: 20px 0 20px 0;
-  }
-  
-  footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+main {
+  margin: 20px 0 20px 0;
+}
+
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <progress class="progress-bar" :max="maxSteps" :value="step"></progress>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+    props: {
+        step: {
+            type: Number,
+            default: 1
+        },
+        maxSteps: {
+            type: Number,
+            default: 100
+        }
+    }
+})
+</script>
+
+<style>
+.progress-bar {
+    width: 100%;
+}
+</style>
